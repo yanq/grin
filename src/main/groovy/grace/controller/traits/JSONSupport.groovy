e9818ba796ec40bfbf82169b5f87240930a72f6b
yan <37767856@qq.com>
2018-09-18 18:19:37 +0800

    因为调用歧义，json() 被解析成了方法。只能复写一层，方便使用。与 html 这样的，体验一致化。

--- src/main/groovy/grace/controller/traits/JSONSupport.groovy
+++ src/main/groovy/grace/controller/traits/JSONSupport.groovy
@@ -17,8 +17,19 @@ trait JSONSupport extends RequestBase {
         return new StreamingJsonBuilder(response.getWriter())
     }
 
-    // todo do something
-//    void json(Object object){
-//        getJson()(object)
-//    }
+    /**
+     * 保持 json 的常规功能
+     * @param object
+     */
+    void json(Map m){getJson()(m)}
+    void json(String name){getJson()(name)}
+    void json(List l){getJson()(l)}
+    void json(Object... args){getJson()(args)}
+    void json(Iterable coll, @DelegatesTo(StreamingJsonBuilder.StreamingJsonDelegate.class) Closure c){getJson()(coll,c)}
+    void json(Collection coll, @DelegatesTo(StreamingJsonBuilder.StreamingJsonDelegate.class) Closure c){getJson()(coll,c)}
+    void json(@DelegatesTo(StreamingJsonBuilder.StreamingJsonDelegate.class) Closure c){getJson()(c)}
+    void json(String name, @DelegatesTo(StreamingJsonBuilder.StreamingJsonDelegate.class) Closure c){getJson()(name,c)}
+    void json(String name, Iterable coll, @DelegatesTo(StreamingJsonBuilder.StreamingJsonDelegate.class) Closure c){getJson()(name,coll,c)}
+    void json(String name, Collection coll, @DelegatesTo(StreamingJsonBuilder.StreamingJsonDelegate.class) Closure c){getJson()(name,coll,c)}
+    void json(String name, Map map, @DelegatesTo(StreamingJsonBuilder.StreamingJsonDelegate.class) Closure callable){getJson()(name,map,callable)}
 }
