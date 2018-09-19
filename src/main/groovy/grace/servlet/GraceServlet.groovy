744f080e047850c0418aea55f6deee55425faf21
yan <37767856@qq.com>
2018-09-19 18:11:31 +0800

    解决中文问题。 似乎 character encoding 不设置，也可以。还是设置了吧。

--- src/main/groovy/grace/servlet/GraceServlet.groovy
+++ src/main/groovy/grace/servlet/GraceServlet.groovy
@@ -20,21 +20,29 @@ class GraceServlet extends GenericServlet {
         HttpServletResponse response = (HttpServletResponse) res
         Route route = Routes.routes.find { it.path == request.requestURI }
         if (route) {
+            //设置默认编码
+            request.setCharacterEncoding('utf-8')
+            response.setCharacterEncoding('utf-8')
+            response.setContentType('text/html;charset=UTF-8')
+
+            //初始化闭包
             Closure closure = route.closure.clone()
             WebRequest webRequest = new WebRequest(request: request, response: response)
             webRequest.controllerName = closure.owner.class.simpleName.uncapitalize()
             closure.delegate = webRequest
             closure.setResolveStrategy(Closure.DELEGATE_ONLY)
 
+            //等待刷新，如果系统在刷新中
             GraceApp.instance.waitingForRefresh()
 
+            //处理
             long start = System.nanoTime()
             Object result
             use(GraceCategory.class) {
                 result = closure()
             }
 
-            log.info("$route.path , ${(System.nanoTime()-start)/1000000} ms")
+            log.info("$route.path , ${(System.nanoTime() - start) / 1000000} ms")
         } else {
             res.writer.write("No route fond for ${request.requestURI}")
         }
