a8e23ad4935e9abc18d19d99da8e5d2374b4b283
yan <37767856@qq.com>
2018-09-24 21:26:29 +0800

    get 初步实现。

--- src/main/groovy/grace/datastore/Entity.groovy
+++ src/main/groovy/grace/datastore/Entity.groovy
@@ -1,14 +1,43 @@
 package grace.datastore
 
+import grace.app.GraceApp
 import groovy.sql.Sql
+import groovy.transform.CompileStatic
 
-trait Entity {
-    Sql sql
+/**
+ * 提供实体类的基本功能
+ */
+trait Entity<D> {
+    //static Class<T> tClass = this.class
+    static Map mapping = [table:'',clommons:[:]]
+    Sql sql //一个示例默认持有一个 sql，懒加载
 
+    /**
+     * sql
+     * @return
+     */
     Sql getSql(){
-        new Sql()
+        if (sql) return sql
+        sql = newSql()
+        return sql
     }
 
+    /**
+     * new sql
+     * @return
+     */
+    static Sql newSql() {
+        return new Sql(GraceApp.instance.dataSource)
+    }
 
-
+    /**
+     * get
+     * todo 如果带着 D ，编译的时候异常，找不到类型 D，这不太科学。
+     * this 返回的是实现的类，这是 trait 的特性。
+     * @param id
+     * @return
+     */
+    static get(Serializable id){
+        println this.declaredFields
+    }
 }
