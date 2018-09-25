0a6343009200f2fc936a265ceefe27b949721631
yan <37767856@qq.com>
2018-09-25 17:12:43 +0800

    完善绑定实体数据。

--- src/main/groovy/grace/datastore/EntityApiImpl.groovy
+++ src/main/groovy/grace/datastore/EntityApiImpl.groovy
@@ -2,36 +2,65 @@ package grace.datastore
 
 import groovy.sql.GroovyRowResult
 
+/**
+ * 实体 api 实现
+ * 实现具体的操作，get save，解析实体类等。
+ */
 class EntityApiImpl {
-    public static final String MAPPING = 'mapping' //mapping 定义实体类与表之间的映射关系
+    public static final String MAPPING = 'mapping' //mapping 定义实体类与表之间的映射关系,table,columns
     public static final List<String> excludeProperties = ['$staticClassInfo', '__$stMC', 'metaClass', '$staticClassInfo$', '$callSiteArray']
 
+    /**
+     * get
+     * 神一样的 get
+     * @param target
+     * @param id
+     * @return
+     */
     static get(Class target, Serializable id) {
         String table = findTableName(target)
         def result = DB.sql.firstRow("select * from ${table} where id=?", id)
-        return bindEntity(target, result)
+        return bindResultToEntity(result, target)
     }
 
+    /**
+     * 获取表名
+     * @param target
+     * @return
+     */
     static String findTableName(Class target) {
         target[MAPPING]?.table ?: DBUtil.toDbName(target.simpleName)
     }
 
+    /**
+     * 获取列映射
+     * @param target
+     * @return
+     */
     static Map columnMap(Class target) {
         target[MAPPING]?.columns ?: [:]
     }
 
-    static bindEntity(Class target, GroovyRowResult result) {
+    /**
+     * 绑定 result 到 实体类
+     * @param result
+     * @param target
+     * @return
+     */
+    static bindResultToEntity(GroovyRowResult result, Class target) {
+        if (!result) return null
+
         def entity = target.newInstance()
-        //List<String> properties = target.declaredFields*.name - excludeProperties
         def columnMap = columnMap(target)
-        result.each {
-            def key = it.key
-            def mappedProperty = columnMap.find { it.value == key }
-            if (mappedProperty) {
-                if (entity.hasProperty(mappedProperty.key)) entity[(mappedProperty.key)] = it.value
+        List<String> properties = target.declaredFields*.name - excludeProperties
+        properties.each {
+            String key = it
+            if (columnMap.containsKey(key)) {
+                key = columnMap[key]
             } else {
-                if (entity.hasProperty(it.key)) entity[(it.key)] = it.value
+                key = DBUtil.toDbName(key)
             }
+            if (result.containsKey(key)) entity[(it)] = result[key]
         }
         return entity
     }
