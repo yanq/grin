65f6a5e5ec20fec0c0b00c9c6b371ab94f457611
yan <37767856@qq.com>
2018-10-01 15:00:17 +0800

    重构 generator，文件名，类扩展名 ide 报错，要避免。

--- src/main/groovy/grace/generate/Generator.groovy
+++ src/main/groovy/grace/generate/Generator.groovy
@@ -12,8 +12,6 @@ import groovy.util.logging.Slf4j
  */
 @Slf4j
 class Generator {
-    static final TEMPLATE_DIR = 'templates'
-    static final CONTROLLER_TEMPLATE = 'Controller.groovy'
     static final templateEngine = new StreamingTemplateEngine()
 
     /**
@@ -21,7 +19,7 @@ class Generator {
      * @param className
      */
     static createDomain(String className) {
-        File template = new File(GraceApp.instance.appDir, TEMPLATE_DIR + '/domain.groovy')
+        File template = new File(templateDir, 'domain')
         File target = new File(GraceApp.instance.domainsDir, ClassUtil.classPath(className) + '.groovy')
         generate(template, target, [packageName: ClassUtil.packageName(className), simpleName: ClassUtil.simpleName(className)])
     }
@@ -34,46 +32,46 @@ class Generator {
      * @return
      */
     static createController(String className) {
-        File template = new File(GraceApp.instance.appDir, TEMPLATE_DIR + '/controller.groovy')
+        File template = new File(templateDir, 'controller')
         File target = new File(GraceApp.instance.controllersDir, ClassUtil.classPath(className) + '.groovy')
         generate(template, target, [className: className])
     }
 
     /**
-     * 控制器处理
+     * 从领域类生成控制器和视图
      * @param className
      * @return
      */
-    static controller(String className) {
-        Class aClass
-
-        //find class
-        try {
-            aClass = Class.forName(className)
-        } catch (Exception e) {
-            log.info("class ${className} is not exist,to generate it")
-        }
-
-        //find template
-        File appDir = GraceApp.instance.appDir
-        File templateDir = new File(appDir, TEMPLATE_DIR)
-        File controllerTemplate = new File(templateDir, CONTROLLER_TEMPLATE)
-        if (!appDir.exists()) {
-            log.error("GraceApp dir ${appDir.absolutePath} not exists ！")
-            return
-        }
-        if (!templateDir.exists()) templateDir.mkdir()
-        if (!controllerTemplate.exists()) {
-            log.error("template ${controllerTemplate.absolutePath} not exists ！")
-            return
-        }
-
-        //生成或者创建
-        if (aClass) {
-            generateController(controllerTemplate, aClass)
-        } else {
-            createController(controllerTemplate, className)
-        }
+    static generateAll(String className) {
+        // Class aClass
+        //
+        // //find class
+        // try {
+        //     aClass = Class.forName(className)
+        // } catch (Exception e) {
+        //     log.info("class ${className} is not exist,to generate it")
+        // }
+        //
+        // //find template
+        // File appDir = GraceApp.instance.appDir
+        // File templateDir = new File(appDir, TEMPLATE_DIR)
+        // File controllerTemplate = new File(templateDir, CONTROLLER_TEMPLATE)
+        // if (!appDir.exists()) {
+        //     log.error("GraceApp dir ${appDir.absolutePath} not exists ！")
+        //     return
+        // }
+        // if (!templateDir.exists()) templateDir.mkdir()
+        // if (!controllerTemplate.exists()) {
+        //     log.error("template ${controllerTemplate.absolutePath} not exists ！")
+        //     return
+        // }
+        //
+        // //生成或者创建
+        // if (aClass) {
+        //     generateController(controllerTemplate, aClass)
+        // } else {
+        //     createController(controllerTemplate, className)
+        // }
     }
 
     /**
@@ -86,11 +84,19 @@ class Generator {
     static generate(File templateFile, File targetFile, Map binding = [:]) {
         if (!targetFile.parentFile.exists()) targetFile.parentFile.mkdirs()
         if (targetFile.exists()) {
-            log.warn("file exists,do nothing! @ $targetFile.absolutePath ")
+            log.warn("file exists,do nothing! @ $targetFile.canonicalPath ")
         } else {
             Template template = templateEngine.createTemplate(templateFile)
             targetFile << template.make(binding).toString().getBytes('utf-8')
-            log.info("generate file @ $targetFile.absolutePath")
+            log.info("generate file @ $targetFile.canonicalPath")
         }
+    }
+
+    /**
+     * 获取模板目录
+     * @return
+     */
+    static File getTemplateDir() {
+        new File(GraceApp.instance.appDir, 'templates')
     }
 }
