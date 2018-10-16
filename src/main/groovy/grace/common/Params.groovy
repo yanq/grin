c69ddffe86585d08ed1c753e143e5ce57f37ee85
Yan <yqp_001@163.com>
2018-10-16 20:45:44 +0800

    支持 java 8 time。

--- src/main/groovy/grace/common/Params.groovy
+++ src/main/groovy/grace/common/Params.groovy
@@ -2,30 +2,47 @@ package grace.common
 
 import groovy.transform.CompileStatic
 import java.text.SimpleDateFormat
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 /**
  * 参数
  */
 @CompileStatic
 class Params extends HashMap<String, Object> {
-    List<String> dateFormats = ['EEE MMM dd HH:mm:ss z yyyy', "yyyy-MM-dd HH:mm:ss.SSS", "yyyy-MM-dd HH:mm", 'yyyy-MM-dd']
-    Locale l = Locale.ENGLISH
+    List<String> dateFormats = ['yyyy-MM-dd', 'yyyyMMdd']
+    List<String> dateTimeFormats = ['EEE MMM dd HH:mm:ss z yyyy', "yyyy-MM-dd HH:mm:ss.SSS", "yyyy-MM-dd HH:mm"]
+    Locale local = Locale.ENGLISH
 
+    /**
+     * 增加更多格式
+     * @param format
+     */
     void addDateFormat(String format) { dateFormats << format }
 
+    void addDateTimeFormat(String format) { dateTimeFormats << format }
+
+    /**
+     * 解析 Date
+     * @param key
+     * @param format
+     * @return
+     */
     Date date(String key, String format = null) {
         def value = super.get(key)
         if (value instanceof Date) return value
-        if (value.toString().contains('月')) l = Locale.SIMPLIFIED_CHINESE
+        if (value.toString().contains('月')) local = Locale.SIMPLIFIED_CHINESE
 
         if (format) {
-            return new SimpleDateFormat(format, l).parse(value.toString())
+            return new SimpleDateFormat(format, local).parse(value.toString())
         }
 
         Date date = null
-        for (int i = 0; i < dateFormats.size(); i++) {
+        List<String> allFormats = dateTimeFormats + dateFormats
+        for (int i = 0; i < allFormats.size(); i++) {
             try {
-                def dateFormat = new SimpleDateFormat(dateFormats[i], l)
+                def dateFormat = new SimpleDateFormat(allFormats[i], local)
                 date = dateFormat.parse(value.toString())
             } catch (Exception e) {
                 // e.printStackTrace()
@@ -34,5 +51,67 @@ class Params extends HashMap<String, Object> {
         }
 
         return date
+    }
+
+    /**
+     * 解析 LocalDate
+     * @param key
+     * @param format
+     * @return
+     */
+    LocalDate localDate(String key, String format = null) {
+        def value = super.get(key)
+        if (value instanceof LocalDate) return value
+
+        if (format) {
+            return LocalDate.parse(value as CharSequence, DateTimeFormatter.ofPattern(format, local))
+        }
+
+        LocalDate date = null
+        for (int i = 0; i < dateFormats.size(); i++) {
+            try {
+                date = LocalDate.parse(value as CharSequence, DateTimeFormatter.ofPattern(dateFormats[i], local))
+            } catch (Exception e) {
+                // e.printStackTrace()
+            }
+        }
+
+        if (date) {
+            return date
+        } else {
+            throw new Exception("no format for local date : $value")
+        }
+
+    }
+
+    /**
+     * 解析 LocalDateTime
+     * @param key
+     * @param format
+     * @return
+     */
+    LocalDateTime localDateTime(String key, String format = null) {
+        def value = super.get(key)
+        if (value instanceof LocalDateTime) return value
+        if (value.toString().contains('月')) local = Locale.SIMPLIFIED_CHINESE
+
+        if (format) {
+            return LocalDateTime.parse(value.toString(), DateTimeFormatter.ofPattern(format, local))
+        }
+
+        LocalDateTime date = null
+        for (int i = 0; i < dateTimeFormats.size(); i++) {
+            try {
+                date = LocalDateTime.parse(value.toString(), DateTimeFormatter.ofPattern(dateTimeFormats[i], local))
+            } catch (Exception e) {
+                // e.printStackTrace()
+            }
+        }
+
+        if (date) {
+            return date
+        } else {
+            throw new Exception("no format for local date time : $value")
+        }
     }
 }
