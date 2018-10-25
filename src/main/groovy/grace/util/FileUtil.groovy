ec6a04cccc98176c78319e023e4d31d7b8038e0c
Yan <yqp_001@163.com>
2018-10-25 14:52:16 +0800

    完善文件处理。前端 Nginx 没有压缩的问题。

--- src/main/groovy/grace/util/FileUtil.groovy
+++ src/main/groovy/grace/util/FileUtil.groovy
@@ -77,8 +77,6 @@ class FileUtil {
         response.setContentType(request.getServletContext().getMimeType(request.requestURI))
         response.setHeader("ETag", etag)
         response.setHeader('Cache-Control', 'public, max-age=' + cacheTime)
-        response.setContentLength((int) target.length());
-        response.setHeader("Accept-Ranges", "bytes");
         response.setHeader("Content-Disposition", "attachment;filename=\" " + target.getName() + " \"");
 
         OutputStream out = response.getOutputStream()
@@ -89,7 +87,8 @@ class FileUtil {
             long fileLength = target.length();
 
             if (request.getHeader("Range") != null) {
-                response.setStatus(HttpServletResponse.SC_PARTIAL_CONTENT);//206
+                response.setHeader("Accept-Ranges", "bytes") //如果放在外面，普通文件也会影响，前段 Nginx gzip 就不起作用了。
+                response.setStatus(HttpServletResponse.SC_PARTIAL_CONTENT);//206  默认是 200，不用专门设置
 
                 def ranges = request.getHeader("Range").replaceAll("bytes=", "").split('-')
                 start = ranges[0].toLong()
