71aa1f9d97b048629860c39b8a2c9b1a64b2aaaa
Yan <yqp_001@163.com>
2019-03-09 15:02:18 +0800

    文件绝对路径。

---
+++ src/main/groovy/grace/util/LinkUtil.groovy
@@ -0,0 +1,19 @@
+package grace.util;
+
+import grace.app.GraceApp;
+
+public class LinkUtil {
+
+    /**
+     * 获取文件的绝对路径
+     * @param file
+     * @return
+     */
+    static String absolute(String file) {
+        if (file && !(file.startsWith('http://') || file.startsWith('https://'))) {
+            return GraceApp.instance.config.serverURL + file
+        } else {
+            return file
+        }
+    }
+}
