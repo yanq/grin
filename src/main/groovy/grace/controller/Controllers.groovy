d4cb89eaed779d80d9001aab546e291d1ac275d8
yan <yqp_001@163.com>
2022-01-22 11:15:37 +0800

    控制器，加载的时候直接缓存方法。

--- src/main/groovy/grace/controller/Controllers.groovy
+++ src/main/groovy/grace/controller/Controllers.groovy
@@ -4,6 +4,10 @@ package grace.controller
 import grace.util.ClassUtil
 import groovy.util.logging.Slf4j
 
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+import java.lang.reflect.Method
+
 /**
  * 控制器工具类
  * 加载控制器，以及处理等。
@@ -12,19 +16,50 @@ import groovy.util.logging.Slf4j
 class Controllers {
 
     Map<String, String> controllerMap = [:]
+    Map<String, Method> methodMap = [:]
 
+    /**
+     * 加载所有的控制器
+     * @param dir
+     */
     void loadControllers(File dir) {
-        controllerMap.clear()
         log.info('start load controllers')
+
+        controllerMap.clear()
+        methodMap.clear()
+
         dir.eachFileRecurse {
-            if (ClassUtil.isJavaClass(it.name)) {
+            if (it.name.matches('.+Controller.groovy')) {
                 def name = ClassUtil.reduce(it.name.split('\\.')[0].uncapitalize())
                 def className = ClassUtil.pathToClassName(it.canonicalPath.replace(dir.canonicalPath, '').substring(1))
                 if (controllerMap.containsKey(name)) throw new Exception("控制器 ${name} 已经存在: ${controllerMap.get(name)},新的 ${className}")
                 controllerMap.put(name, className)
             }
         }
+
+        controllerMap.each { name, className ->
+
+            Class.forName(className).getDeclaredMethods()
+                    .findAll {
+                        def methodName = it.name
+                        !['$', 'super$'].find { methodName.startsWith(it) }
+                    }
+                    .each {
+                        methodMap.put("${name}-${it.name.uncapitalize()}", it)
+                    }
+        }
+
         log.info("loaded, controllers ${controllerMap.keySet()}")
+    }
+
+    /**
+     * 执行 action
+     * @param controllerName
+     * @param actionName
+     * @param id
+     */
+    void executeAction(HttpServletRequest request, HttpServletResponse response, String controllerName, String actionName, String id) {
+
     }
 
 }
--- src/test/groovy/grace/controller/ControllersTest.groovy
+++ src/test/groovy/grace/controller/ControllersTest.groovy
@@ -4,6 +4,6 @@ class ControllersTest extends GroovyTestCase {
     void testLoadControllers() {
         println(new File('.').absolutePath)
         def c = new Controllers()
-        c.loadControllers(new File("/Users/yan/projects/grace-dev/grace-example/grace-app/controllers"))
+        c.loadControllers(new File("/Users/yan/projects/grace-dev/grace/src/main/groovy"))
     }
 }
