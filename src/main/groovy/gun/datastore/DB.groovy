e9c870d8d7aa2516e93a1472a60119bfd1303625
yan <yqp_001@163.com>
2022-11-13 14:26:35 +0800

    修改 db，去掉对事务的支持。知识不够还瞎实现，现在都不知道是否真的可用。

--- src/main/groovy/gun/datastore/DB.groovy
+++ src/main/groovy/gun/datastore/DB.groovy
@@ -7,26 +7,17 @@ import javax.sql.DataSource
 
 /**
  * DB 数据库
- * todo 验证完善，表更新 等
  */
 @Slf4j
 class DB {
-    static DataSource dataSource //datasource,提供一个配置的入口，方便 gun 外部使用。
-    static ThreadLocal<Sql> localSql = new ThreadLocal<>() // 线程内的 sql，主要用来控制事务
+    static DataSource dataSource // datasource,提供一个配置的入口，方便 gun 外部使用。
 
     /**
      * 获取 sql
      * @return
      */
     static Sql getSql() {
-        Sql sql = localSql.get()
-        if (sql) {
-            log.debug('use local sql')
-        } else {
-            log.debug('create new sql')
-            sql = new Sql(dataSource)
-        }
-        return sql
+        new Sql(dataSource)
     }
 
     /**
@@ -37,29 +28,10 @@ class DB {
     static withSql(@DelegatesTo(SQL) Closure closure) {
         Sql sql = getSql()
         def result = closure.call(sql)
-        if (sql.connection?.autoCommit) sql.close() //自动提交的时候才关闭，如事务中，单独的执行不能关闭 sql
+        sql.close()
         return result
     }
 
-    /**
-     * with transaction
-     * @param closure
-     * @return
-     */
-    static withTransaction(@DelegatesTo(SQL) Closure closure) {
-        Sql sql = getSql()
-        localSql.set(sql)
-        try {
-            def result
-            sql.withTransaction {
-                result = closure.call(sql)
-            }
-            return result
-        } finally {
-            localSql.remove()
-            sql.close()
-        }
-    }
 
     /**
      * 用作闭包代理，便于 IDE 提示。
