171da636d6795b9eaa520b127590c0af3493e8c5
yan <yqp_001@163.com>
2022-11-17 11:29:42 +0800

    模板生成，完善约束处理。

--- src/main/groovy/grin/generate/FormGenerator.groovy
+++ src/main/groovy/grin/generate/FormGenerator.groovy
@@ -11,15 +11,14 @@ class FormGenerator extends Generator {
 
     static String generateForm(Class entityClass) {
         List<String> props = Utils.findPropertiesToPersist(entityClass) - 'id'
-        Map constraints = entityClass.constraints
 
         List<String> result = []
         props.each {
             Class propClass = entityClass.getDeclaredField(it)?.type
-            if (propClass in [List,Map]) return //有些类型明确处理不了的，略过
+            if (propClass in [List, Map]) return // 有些类型明确处理不了的，略过
             String type = types[propClass.name] ?: 'text'
             if (propClass) {
-                result << generateItem(entityClass, it, type, constraints[it]) ?: ''
+                result << generateItem(entityClass, it, type, findConstraints(entityClass,it)) ?: ''
             }
         }
         return result.join('\n')
@@ -28,11 +27,19 @@ class FormGenerator extends Generator {
     static String generateItem(Class entityClass, String propName, String propType, Map constraints) {
         if (entityClass.getDeclaredField(propName).type.interfaces.contains(Entity)) {// entity
             entity(entityClass, propName, propType, constraints)
-        } else if (constraints?.find { it.class.simpleName == 'InList' }) { // select
+        } else if (constraints.inList) { // select
             select(entityClass, propName, propType, constraints)
         } else {// text
             input(entityClass, propName, propType, constraints)
         }
+    }
+
+    static Map<String, Object> findConstraints(Class entityClass, String propName) {
+        def result = [:]
+        result['nullable'] = Utils.getEntityConstraintValue(entityClass, propName, 'Nullable')
+        result['maxLength'] = Utils.getEntityConstraintValue(entityClass, propName, 'MaxLength')
+        result['inList'] = Utils.getEntityConstraintValue(entityClass, propName, 'InList')
+        return result
     }
 
     static input(Class entityClass, String propName, String propType, Map constraints) {
